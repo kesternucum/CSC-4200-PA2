e7b72ce50454f381d376338f46e2805f1788cbba
Kester Nucum <knucum42@students.tntech.edu>
2023-02-19 01:32:28 -0600

    Figured out command line parsing and started on packets in client side

--- lightclient.py
+++ lightclient.py
@@ -1,10 +1,27 @@
 import socket
+import argparse
+import struct
 
-SERV_ADDR = "127.0.0.1"
-SERV_PORT = 5432
+SERV_ADDR = " "
+SERV_PORT = 0
+LOGFILE = " "
+
+parser = argparse.ArgumentParser();
+parser.add_argument('-s', action='store', type=str, help='IP address of server')
+parser.add_argument('-p', action='store', type=int, help='port server listens on')
+parser.add_argument('-l', action='store', type=str, help='log file that record actions')
+
+args = parser.parse_args()
+
+SERV_ADDR = args.s
+SERV_PORT = args.p
+LOGFILE = args.l
+
+msg1 = struct.pack("!III8s",17,0,5,b"Hello")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((SERV_ADDR, SERV_PORT))
-    client_socket.sendall(b"Hello World!")
+    client_socket.sendall(msg1)
+    #client_socket.sendall(b"Hello World!")
     return_data = client_socket.recv(1024)
     print(return_data)
--- lightserver.py
+++ lightserver.py
@@ -2,36 +2,42 @@ import socket
 import argparse
 
 def server():
-    parser = argparse.ArgumentParser(description = 'set up server')
-    
+    HOST = "127.0.0.1" # Already pre-set
     PORT = 0
-    parser.add_argument('-p', dest = PORT, action = 'store')
-    
-    HOST = "127.0.0.1"
-    #PORT = 5432
-    
+    LOGFILE = " "
+
+    parser = argparse.ArgumentParser();
+    parser.add_argument('-p', action='store', type=int, help='port server listens on')
+    parser.add_argument('-l', action='store', type=str, help='log file that record actions')
+
+    args = parser.parse_args()
+
+    PORT = args.p
+    FILE_LOCATION = args.l
+
     mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Created socket object")
-    #mysocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
+    # print("Created socket object")
+    # mysocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     mysocket.bind((HOST, PORT))
-    print("Bound to {}, {}".format(HOST, PORT))
+    # print("Bound to {}, {}".format(HOST, PORT))
     mysocket.listen()
-    print("Listening")
+    # print("Listening")
     INCONN, INADDR = mysocket.accept()
-    print("Incoming connection {}{} =".format(INCONN, INADDR))
-    
+    print("Received connection from {}{} =".format(INCONN, INADDR)) #
+
     with INCONN: #while incoming connection is active
-        print("Incoming connection {}{} =".format(INCONN, INADDR))
+
         while True:
             data = INCONN.recv(1024)
             print("Received data of length {}".format(len(data)))
+            # Removed until it is deemed needed to have a break statement
             if not data:
-                  print("Communication ended")
-                  break
-                  
+                print("Communication ended")
+                break
+
             print("Echoing back")
             INCONN.sendall(data)
-                  
+
 if __name__=='__main__':
     server();
