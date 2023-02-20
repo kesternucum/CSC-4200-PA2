e843b04d41cbd39630f8e43559ba922917e0ba9c
Kester Nucum <knucum42@students.tntech.edu>
2023-02-19 23:33:45 -0600

    Completed most of the client and server without LED enable

--- lightclient.py
+++ lightclient.py
@@ -6,7 +6,7 @@ SERV_ADDR = " "
 SERV_PORT = 0
 LOGFILE = " "
 
-parser = argparse.ArgumentParser();
+parser = argparse.ArgumentParser()
 parser.add_argument('-s', action='store', type=str, help='IP address of server')
 parser.add_argument('-p', action='store', type=int, help='port server listens on')
 parser.add_argument('-l', action='store', type=str, help='log file that record actions')
@@ -17,11 +17,47 @@ SERV_ADDR = args.s
 SERV_PORT = args.p
 LOGFILE = args.l
 
-msg1 = struct.pack("!III8s",17,1,8,b'HELLO')
+pkt_format = "!III8s"
+
+pkt1 = struct.pack(pkt_format,17,0,len(b'HELLO'),b'HELLO')
+pkt2 = struct.pack(pkt_format,17,1,len(b'LIGHTON'),b'LIGHTON')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+
     client_socket.connect((SERV_ADDR, SERV_PORT))
-    client_socket.sendall(msg1)
-    #client_socket.sendall(b"Hello World!")
-    return_data = client_socket.recv(8).decode("utf-8")
-    print("Received Message: {}".format(return_data))
+
+    # Always start client by sending hello packet to server
+    print("Sending Hello Packet")
+    client_socket.sendall(pkt1)
+
+    header = struct.unpack("!III", client_socket.recv(12))
+    msg = client_socket.recv(8).decode("utf-8").strip('\0')
+    version = header[0]
+    msg_type = header[1]
+    msg_length = header[2]
+
+    print("Received Data - Version: {}, Message Type: {}, Length: {}".format(header[0], header[1], header[2]))
+
+    if version != 17:
+        print("VERSION MISMATCH")
+    else:
+        print("VERSION ACCEPTED")
+        print("Received Message: {}".format(msg))
+        client_socket.sendall(pkt2)
+
+        header = struct.unpack("!III", client_socket.recv(12))
+        msg = client_socket.recv(8).decode("utf-8").strip('\0')
+        version = header[0]
+        msg_type = header[1]
+        msg_length = header[2]
+        print("Received Data - Version: {}, Message Type: {}, Length: {}".format(header[0], header[1], header[2]))
+
+        if version != 17:
+            print("VERSION MISMATCH")
+        else:
+            print("VERSION ACCEPTED")
+            print("Received Message: {}".format(msg))
+            print("Command successful")
+            print("Closing socket")
+            client_socket.shutdown(socket.SHUT_RDWR)
+            client_socket.close()
--- lightserver.py
+++ lightserver.py
@@ -1,5 +1,12 @@
 import socket
 import argparse
+import struct
+
+pkt_format = "!III8s"
+pkt_version = 17
+
+pkt1 = struct.pack(pkt_format,17,0,len(b'HELLO'),b'HELLO')
+pkt2 = struct.pack(pkt_format,17,1,len(b'SUCCESS'),b'SUCCESS')
 
 def server():
     HOST = "127.0.0.1" # Already pre-set
@@ -9,36 +16,50 @@ def server():
     parser = argparse.ArgumentParser();
     parser.add_argument('-p', action='store', type=int, help='port server listens on')
     parser.add_argument('-l', action='store', type=str, help='log file that record actions')
-
     args = parser.parse_args()
-
     PORT = args.p
     FILE_LOCATION = args.l
 
     mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # print("Created socket object")
-    # mysocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     mysocket.bind((HOST, PORT))
-    # print("Bound to {}, {}".format(HOST, PORT))
     mysocket.listen()
-    # print("Listening")
+
     INCONN, INADDR = mysocket.accept()
     print("Received connection from (IP, PORT): {}".format(INADDR))
 
-    with INCONN: #while incoming connection is active
+    with INCONN: # while incoming connection is active
 
         while True:
             header = INCONN.recv(12)
-            data = INCONN.recv(8)
-            print("Received data of length {}".format(len(data)))
-            # Removed until it is deemed needed to have a break statement
-            if not data:
-                print("Communication ended")
-                break
+            msg = INCONN.recv(8)
 
-            print("Echoing back")
-            INCONN.sendall(data)
+            if header and msg:
+                header = struct.unpack("!III", header)
+                version = header[0]
+                msg_type = header[1]
+                msg_length = header[2]
+                print("Received Data - Version: {}, Message Type: {}, Length: {}".format(version, msg_type, msg_length))
+                msg = msg.decode("utf-8").strip('\0')
+
+                if version != 17:
+                    print("VERSION MISMATCH")
+                else:
+                    print("VERSION ACCEPTED")
+                    # first checks if the packet is a hello
+
+                    if msg == "HELLO":
+                        INCONN.sendall(pkt1)
+                    else:
+                        if msg_type == 1:
+                            # turns on LED ... TODO: Add this
+                            print("EXECUTING SUPPORTED COMMAND: {}".format(msg))
+                        elif msg_type == 2:
+                            # turns off LED ... TODO: Add this
+                            print("EXECUTING SUPPORTED COMMAND: {}".format(msg))
+                        else:
+                            print("IGNORING UNKNOWN COMMAND: {}".format(msg))
+                        INCONN.sendall(pkt2)
+                        print("Returning SUCCESS")
 
 if __name__=='__main__':
     server();
